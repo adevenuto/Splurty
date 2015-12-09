e61235850b082ad749fac37336bc8d09dc258572
adevenuto <anthonydevenuto@gmail.com>
2015-12-09 16:33:26 +0000

    added quote test

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -10,4 +10,19 @@ class QuotesControllerTest < ActionController::TestCase
     get :show, :id => 'OMG'
     assert_response :not_found
   end
+  test "create a quote success" do
+    quote = FactoryGirl.create(:quote)
+    assert_difference 'Quote.count' do
+      post :create, {:quote =>{
+        :author => 'Gary Vaynerchuk',
+        :saying => 'I love people, and the hustle.'
+        }
+      }
+    end
+    assert_redirected_to root_path
+  end
+
+  test "create a quote validation error"do
+
+  end
 end
